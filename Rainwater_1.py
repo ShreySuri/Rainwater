98f454e2d02f5af0375d7def4bbd8cdc48524b3a
ShreySuri <shreysuri5@gmail.com>
2022-08-11 17:57:32 -0500

    Update Rainwater_1.py

--- Rainwater_1.py
+++ Rainwater_1.py
@@ -4,6 +4,7 @@ def capturing_rainwater(list_1):
     first_bound = 0
     first_val = list[0]
     length = len(list_1)
+    vacancy = 0
     for i in range (1, length):
         second_val = list_1[i]
         if second_val > first_val:
@@ -13,4 +14,15 @@ def capturing_rainwater(list_1):
             else:
                 lower_value = list_1[second_bound]
             total_ground_height = 0
-            for i in range (first_bound + 1, second_bound):
+            columns = 0
+            for j in range (first_bound + 1, second_bound):
+                total_ground_heigth = total_ground_height + list_1[j]
+                columns = coulumns + 1
+
+            vacancy = vacancy + lower_value * columns - total_ground height
+        else:
+            toggle = True
+
+    return(vacancy)
+
+
