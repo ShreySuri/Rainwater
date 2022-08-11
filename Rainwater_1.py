9ac64068185dfa716eefadb56618ae4ebef985e5
ShreySuri <shreysuri5@gmail.com>
2022-08-11 17:55:13 -0500

    Update Rainwater_1.py

--- Rainwater_1.py
+++ Rainwater_1.py
@@ -8,5 +8,9 @@ def capturing_rainwater(list_1):
         second_val = list_1[i]
         if second_val > first_val:
             second_bound = i
-            if 
+            if list_1[second_bound] > list_1[first_bound]:
+                lower_value = list_1[first_bound]
+            else:
+                lower_value = list_1[second_bound]
+            total_ground_height = 0
             for i in range (first_bound + 1, second_bound):
