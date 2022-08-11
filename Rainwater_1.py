d268ac7f8572356e30bc6321933d5ef03cd14f09
ShreySuri <shreysuri5@gmail.com>
2022-08-11 17:38:04 -0500

    Create Rainwater_1.py

---
+++ Rainwater_1.py
@@ -0,0 +1,12 @@
+
+
+def capturing_rainwater(list_1):
+    first_bound = 0
+    first_val = list[0]
+    length = len(list_1)
+    for i in range (1, length):
+        second_val = list_1[i]
+        if second_val > first_val:
+            second_bound = i
+            if 
+            for i in range (first_bound + 1, second_bound):
